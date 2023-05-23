c7f5bff664c5103c42ef84cc069232565af5c6bf
cgg734 <90397399+cgg734@users.noreply.github.com>
2023-05-23 14:44:18 +0200

    Update PlayingStandTest.java

--- test/net/sourceforge/cruisecontrol/sampleproject/connectfour/PlayingStandTest.java
+++ test/net/sourceforge/cruisecontrol/sampleproject/connectfour/PlayingStandTest.java
@@ -42,7 +42,7 @@ public class PlayingStandTest extends TestCase {
 
     public void testFourConnected() throws GameOverException {
         PlayingStand stand = new PlayingStand();
-        assertFalse(stand.areFourConnected());
+        assertTrue(stand.areFourConnected());
 
         stand.dropRed(0);
         stand.dropBlack(6);
@@ -196,7 +196,7 @@ public class PlayingStandTest extends TestCase {
 
         fillWholeStandWithoutWinner(stand);
 
-        assertTrue(stand.areFourConnected());  // Otra rotura  rehecha (assertFalse) 
+        assertFalse(stand.areFourConnected());  // Otra rotura  rehecha (assertFalse) 
         assertTrue(stand.isGameOver());
         assertNull(stand.getWinner());
 
